bdf1cbcf2452b11941d10a007726218fac2ceb4c
hitlar240 <tanvir22228@gmail.com>
2023-12-04 16:15:21 +0600

    new

--- Phitron-OOP/Mid_Term.py
+++ Phitron-OOP/Mid_Term.py
@@ -1,5 +1,5 @@
 class Star_Cinema:
-    hall_list = []
+    hall_list = [] # list of (Hall)objects
 
     @classmethod
     def entry_hall(cls,hall):
@@ -7,35 +7,90 @@ class Star_Cinema:
 
 
 class Hall(Star_Cinema):
-    seats = {} # doctionary {key: value, ...}
-    show_list = [] # list of tuples [(...,...,...),(,...)]
     def __init__(self, rows, cols, hall_no):
         self.rows = rows
         self.cols = cols
         self.hall_no = hall_no
+        self.seats = {}
+        self.__show_list = [] 
         self.entry_hall(self)
 
-    @classmethod
-    def entry_show(cls, id, movie_name, time):
-        # self.id = id
-        # self.movie = movie_name
-        # self.time = time
-        cls.show_list.append((id,movie_name,time))
+    def entry_show(self, id, movie_name, time):
+        self.__show_list.append((id,movie_name,time))
+        self.seats[id] = [[0 for j in range(self.cols)] for i in range(self.rows)]
+
+    def view_show_list(self):
+        for show in self.__show_list:
+            print(f'Movie:{show[1]} ID:{show[0]} Time:{show[2]}')
+
+    def view_available_seats(self,id):
+        for row in self.seats[id]:
+            print(row)
+
+    def book_seats(self,id,seat):
+        for st in seat:
+            self.seats[id][st[0]][st[1]] = 1
+
+hall_01 = Hall(10,7,'A-111')
+hall_01.entry_show('B101','Molla Barir Bou','04/12/23 at 3.00pm')
+hall_01.entry_show('B102','Aynabaji','04/12/23 at 5.00pm')
+
+hall_02 = Hall(12,8,'B-123')
+hall_02.entry_show('E201','Spiderman','04/12/23 at 5.00pm')
+hall_02.entry_show('E202','Superman','04/12/23 at 7.00pm')
+
+hall_03 = Hall(8,10,'C-240')
+hall_03.entry_show('H301','PK','04/12/23 at 7.00pm')
+hall_03.entry_show('H302','KGF','04/12/23 at 9.00pm')
 
 
-hall_01 = Hall(10,20,111)
-hall_02 = Hall(20,20,123)
-hall_03 = Hall(10,10,240)
+while(True):
+    print('1. Running Shows')
+    print('2. Avaiable Seats')
+    print('3. Book Ticket')
+    print('4. Exit')
 
+    choise = int(input('Enter Option: '))
+    if choise == 1:
+        for hall in Hall.hall_list:
+            print('---------------------------------')
+            print(f'Hall No: {hall.hall_no}')
+            hall.view_show_list()
+            print('---------------------------------')
 
-Hall.entry_show('B101','Molla Barir Bou','04/12/23 at 3.00pm')
-Hall.entry_show('E102','Spiderman','04/12/23 at 5.00pm')
-Hall.entry_show('H103','PK','04/12/23 at 7.00pm')
+    elif choise == 2:
+        flag = False
+        id = input('Enter Show ID: ')
+        for hall in Hall.hall_list:
+            if id in hall.seats.keys():
+                flag = True
+                hall.view_available_seats(id)
+        if flag == False:
+            print('Invalid ID!')
 
+    elif choise == 3:
+        flag = False
+        id = input('Enter Show ID: ')
+        for hall in Hall.hall_list:
+            if id in hall.seats.keys():
+                flag = True
+                seats = []
+                n = int(input("Number of seats?: "))
+                while(n != 0):
+                    row = int(input("Enter Row:"))
+                    col = int(input("Enter Col:"))
+                    if row > hall.rows or col > hall.cols or row < 0 or col < 0:
+                        print("Envalid row or col!")
+                        continue
+                    if hall.seats[id][row][col] == 1:
+                        print("Already Booked!")
+                        continue
+                    seats.append((row,col))
+                    n -= 1
+                hall.book_seats(id,seats)
+                print('Booked Successfully')
+        if flag == False:
+            print('Invalid ID!')
 
-
-print(hall_02.show_list)
-
-for hall in Star_Cinema.hall_list:
-    print(hall.hall_no)
-
+    else:
+        break
--- Phitron-OOP/W01-dictionary.py
+++ Phitron-OOP/W01-dictionary.py
@@ -2,13 +2,13 @@
 #                 mutable
 # """
 
-# id = {'Name': "Tom", "Age": 23, 'Address': "Islampur"}
+id = {'Name': "Tom", "Age": 23, 'Address': "Islampur"}
 
 # print(id)
 # print(id["Name"])
 # id["Name"] = "Tanvir"
-# print(id.keys())
-# print(id.values())
+print(id.keys())
+print(id.values())
 
 # id["Profession"] = "bekar"
 # del id["Address"]
@@ -17,9 +17,9 @@
 # for key, val in id.items():
 #     print(key, val)
 
-mp = {}
-mp[2] = 5
-mp[3] = 9
-mp[4] = 12
+# mp = {}
+# mp[2] = 5
+# mp[3] = 9
+# mp[4] = 12
 
-print(mp)
+# print(mp)
